02394e6feaf49fc59165303a19b90376be00239b
rifle007 <mrifai025@gmail.com>
2017-04-13 16:47:43 +0700

    enable msm hotplug, disable other

--- arch/arm/mach-msm/alucard_hotplug.c
+++ arch/arm/mach-msm/alucard_hotplug.c
@@ -69,7 +69,7 @@ static struct hotplug_tuners {
 #endif
 } hotplug_tuners_ins = {
 	.hotplug_sampling_rate = 30,
-	.hotplug_enable = 1,
+	.hotplug_enable = 0,
 	.min_cpus_online = 1,
 	.maxcoreslimit = 2,
 	.maxcoreslimit_sleep = 1,
